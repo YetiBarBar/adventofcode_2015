0acede1a0995d5f2065eaf831f5075d15ab7350c
YetiBarBar <yetibarbar@gmail.com>
2022-10-09 12:10:20 +0200

    Fix VCS before working on day 8

--- src/day4.rs
+++ src/day4.rs
@@ -1,8 +1,8 @@
 use adventofcode_tooling::AocError;
 
 #[must_use]
-pub fn process(data: &str, goal: &str) -> usize {
-    for idx in 1.. {
+pub fn process(data: &str, goal: &str, start: usize) -> usize {
+    for idx in start.. {
         let data = format!("{}{}", data, idx);
         let hashed = format!("{:?}", md5::compute(data.as_bytes()));
         if hashed.starts_with(goal) {
@@ -19,7 +19,7 @@ pub fn process(data: &str, goal: &str) -> usize {
 /// can't produce error
 #[must_use]
 pub fn part_1(data: &str) -> usize {
-    process(data, "00000")
+    process(data, "00000", 1)
 }
 
 /// Process data for a given step
@@ -28,8 +28,8 @@ pub fn part_1(data: &str) -> usize {
 ///
 /// can't produce error
 #[must_use]
-pub fn part_2(data: &str) -> usize {
-    process(data, "000000")
+pub fn part_2(data: &str, start: usize) -> usize {
+    process(data, "000000", start)
 }
 
 /// Process solutions for day 1
@@ -39,9 +39,9 @@ pub fn part_2(data: &str) -> usize {
 /// May fail if input data cannot be read
 pub fn main() -> Result<(), AocError> {
     let now = std::time::Instant::now();
-
-    println!("Part 1: {:?}", part_1("bgvyzdsv"));
-    println!("Part 2: {:?}", part_2("bgvyzdsv"));
+    let p1 = part_1("bgvyzdsv");
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", part_2("bgvyzdsv", p1));
     let elapsed = now.elapsed();
     println!("Exec time: {} \u{b5}s", elapsed.as_micros());
     Ok(())
