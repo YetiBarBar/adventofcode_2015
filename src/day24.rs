a8e6aff65260c5619a2c515a1a565706a89b9b1d
YetiBarBar <yetibarbar@gmail.com>
2022-10-31 21:12:07 +0100

    Add solution for day 24

--- src/day24.rs
+++ src/day24.rs
@@ -1,3 +1,30 @@
+use itertools::Itertools;
+
+fn try_permutation(input: &[usize], size: usize, goal: usize) -> Option<usize> {
+    input
+        .iter()
+        .permutations(size)
+        .filter(|candidate| candidate.iter().copied().sum::<usize>() == goal)
+        .map(|candidate| candidate.iter().copied().product::<usize>())
+        .min()
+}
 fn main() {
-    todo!()
+    let input: Vec<usize> = include_str!("../data/day24.data")
+        .lines()
+        .map(str::parse::<usize>)
+        .map(Result::unwrap)
+        .collect();
+
+    println!("Part 1: {}", solve(&input, 3));
+    println!("Part 2: {}", solve(&input, 4));
+}
+
+fn solve(input: &[usize], parts: usize) -> usize {
+    let goal = input.iter().sum::<usize>() / parts;
+    for idx in 0..input.len() {
+        if let Some(value) = try_permutation(input, idx, goal) {
+            return value;
+        }
+    }
+    unreachable!()
 }
